ee9ca411bc4ad3cfd1ec7c9b534e1adbf4a36ab8
Anjana Thrishakya <anjanathrishakya@gmail.com>
2023-08-06 13:07:19 +0530

    Customer Add

--- src/supermarket/layerd/service/custom/impl/CustomerServiceImpl.java
+++ src/supermarket/layerd/service/custom/impl/CustomerServiceImpl.java
@@ -4,7 +4,10 @@
  */
 package supermarket.layerd.service.custom.impl;
 
+import supermarket.layerd.dao.DaoFactory;
+import supermarket.layerd.dao.custom.CustomerDao;
 import supermarket.layerd.dto.CustomerDto;
+import supermarket.layerd.entity.CustomerEntity;
 import supermarket.layerd.service.custom.CustomerService;
 
 /**
@@ -12,10 +15,19 @@ import supermarket.layerd.service.custom.CustomerService;
  * @author anjanathrishakya
  */
 public class CustomerServiceImpl implements CustomerService{
+    
+    CustomerDao customerDao = (CustomerDao) DaoFactory.getInstance().getDao(DaoFactory.DaoTypes.CUSTOMER);
 
     @Override
-    public String addCustomer(CustomerDto customerDto) throws Exception {
-        return null;
+    public String addCustomer(CustomerDto dto) throws Exception {
+        CustomerEntity entity = new CustomerEntity(dto.getId(),
+                dto.getTitle(), dto.getName(), dto.getDob(),
+                dto.getSalary(), dto.getAddress(), dto.getCity(),
+                dto.getProvince(), dto.getZip());
+        
+        
+        
+        return customerDao.add(entity) ? "Success" : "Fail";
     }
     
 }
